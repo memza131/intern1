86bd4591a6aa4cfb69303bf5442c853f9bdb2376
memza131 <memza131@gmail.com>
2021-08-24 12:32:22 +0700

    jj

--- setup.sh
+++ setup.sh
@@ -1,13 +1,9 @@
 mkdir -p ~/.streamlit/
 
-echo "[theme]
-base="light"
-primaryColor="#e4ad86"
-secondaryBackgroundColor="#fbe8e8"
-textColor="#908263"
-font = "sans serif"
-[server]
-headless = true
-port = $PORT
-enableCORS = false
+echo "\
+[server]\n\
+headless = true\n\
+port = $PORT\n\
+enableCORS = false\n\
+\n\
 " > ~/.streamlit/config.toml
